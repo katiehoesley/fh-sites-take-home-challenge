a6c73c0089645443a0f86882bad4ea9496eaaefe
Katie Hoesley <katiehoesley@gmail.com>
2019-04-16 16:01:12 -0600

    test suite finished

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -1,10 +1,90 @@
 class PokerHand {
-  constructor() {}
-
-  getRank() {
-    // Implement poker hand ranking
-    return 'Royal Flush';
+  constructor(hand) {
+    this.hand = hand
   }
+
+getRank(){
+  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const storage = {}; 
+  
+  // const suits = {
+  //   'd' : 1, 
+  //   's' : 2, 
+  //   'c' : 3, 
+  //   'h' : 4
+  // } 
+  
+  const cardTupels = this.hand.split(' ').map((el) => el.slice(0, 2)==='10'? el = [el.slice(0, 2), el.slice(2)] : el.split('')) 
+  // const ranks = cardTupels.map((el) => el = [values.indexOf(el[0]), suits[el[1]]]) //use this for straight & straight flush
+  // return ranks
+
+
+  for(var i = 0; i < cardTupels.length; i++) {
+    if(storage[cardTupels[i][0]] === undefined) {
+      storage[cardTupels[i][0]] = 1
+    } else {
+      storage[cardTupels[i][0]] ++
+    }
+  }
+
+  const cardFrequency = Object.values(storage)
+  const cardValue = Object.keys(storage)
+
+  let isFlush = () => {
+    const cardTupels = this.hand.split(' ').map((el) => el.slice(0, 2)==='10'? el = [el.slice(0, 2), el.slice(2)] : el.split('')) 
+    const values = cardTupels.map((el) => el = el[1]); 
+
+    for(var i = 0; i < values.length; i++) {
+    let allSame = 0; 
+      if(values[i][1] !== values[i+1][1]) {
+        allSame++
+      }
+      allSame === 0 ? isFlush = true : isFlush = false
+    }
+  }
+
+  const isStraight = () => {
+    const cardValues = this.hand.map((el) => el = values.indexOf(el)); 
+    var counter = 0; 
+    for(var i = 0; i < cardValues.length; i++) {
+      if(cardValues[i] +1 !== cardValues[i+1]) {
+        counter +=1
+      }
+      return counter === 0 ? isStraight = true : isStraight = false;
+    }
+  }
+  
+
+  const allRoyal = () => {
+    return cardValue.join('').sort() === '10AJKQ' ? allRoyal = true : allRoyal = false; 
+    
+  }
+
+
+    if(isFlush && allRoyal && !isStraight) {
+      return 'Royal Flush'
+    } else if (isFlush && isStraight && !allRoyal) {
+      return 'Straight Flush'
+    } else if (isFlush && !isStraight && !allRoyal) {
+      return 'Flush'
+    } else if (isStraight && !allRoyal && !isFlush) {
+      return 'Straight'
+    } else if(cardFrequency.includes(4)) {
+      return 'Four of a Kind'
+    } else if (cardFrequency.includes(3) && !cardFrequency.includes(2)){
+      return 'Three of a Kind'
+    } else if (cardFrequency.toString() === '3,2' || cardFrequency.toString()==='2,3') {
+      return 'Full House'
+    } else if (cardFrequency.sort().toString() === '1,2,2') {
+      return 'Two Pair'
+    } else if (cardFrequency.sort().toString() === '1,1,1,2') {
+      return 'One Pair'
+    } else {
+      return 'High Card'
+     }
+  }
+
 }
+
 
 module.exports = PokerHand;
--- challenge-js/test/pokerHandTest.js
+++ challenge-js/test/pokerHandTest.js
@@ -1,9 +1,7 @@
 var assert = require('assert');
 var PokerHand = require('../pokerHand.js');
 
-/**
- * test
- */
+
 describe('Rank a Royal Flush', function() {
   it('Return royal flush when hand given', function() {
     var hand = new PokerHand('As Ks Qs Js 10s');
@@ -11,31 +9,33 @@ describe('Rank a Royal Flush', function() {
   });
 });
 
-/**
- * test
- */
-describe('Rank a Pair', function() {
-  it('Return one pair when hand given', function() {
-    var hand = new PokerHand('Ah As 10c 7d 6s');
 
-    assert.equal(hand.getRank(), 'One Pair');
+describe('Rank a Straight Flush', function() {
+  it('Return straight flush when hand given', function() {
+    var hand = new PokerHand('3h, 4h, 5h, 6h, 7h');
+    assert.equal(hand.getRank(), 'Straight Flush');
   });
 });
 
-/**
- * test
- */
-describe('Rank Two Pair', function() {
-  it('Return two pair when hand given', function() {
-    var hand = new PokerHand('Kh Kc 3s 3h 2d');
 
-    assert.equal(hand.getRank(), 'Two Pair');
+describe('Four of a Kind', function() {
+  it('Return four of a kind when hand given', function() {
+    var hand = new PokerHand('As As As As 10s');
+    assert.equal(hand.getRank(), 'Four of a Kind');
   });
 });
 
-/**
- * test
- */
+
+describe('Rank Full House', function() {
+  var hand = new PokerHand('Kh Ks Kc Qh Qh');
+
+  it('Return full house when hand given', function() {
+    assert.equal(hand.getRank(), 'Full House');
+  });
+});
+
+
+
 describe('Rank A Flush', function() {
   var hand = new PokerHand('Kh Qh 6h 2h 9h');
 
@@ -44,4 +44,50 @@ describe('Rank A Flush', function() {
   });
 });
 
-// More tests go here
+
+
+describe('Rank A Straight', function() {
+  var hand = new PokerHand('10h 9h 8h 7h 6h');
+
+  it('Return straight when hand given', function() {
+    assert.equal(hand.getRank(), 'Straight');
+  });
+});
+
+
+
+describe('Rank A Three Of A Kind', function() {
+  var hand = new PokerHand('Kh Ks Kc 2h 9h');
+
+  it('Return Three of a Kind when hand given', function() {
+    assert.equal(hand.getRank(), 'Three of a Kind');
+  });
+});
+
+
+describe('Rank Two Pair', function() {
+  it('Return two pair when hand given', function() {
+    var hand = new PokerHand('Kh Kc 3s 3h 2d');
+    
+    assert.equal(hand.getRank(), 'Two Pair');
+  });
+});
+
+
+describe('Rank a Pair', function() {
+  it('Return one pair when hand given', function() {
+    var hand = new PokerHand('Ah As 10c 7d 6s');
+
+    assert.equal(hand.getRank(), 'One Pair');
+  });
+});
+
+
+describe('Rank High Card', function() {
+  var hand = new PokerHand('Kh Qs Jc 2h 9h');
+
+  it('Return high card when hand given', function() {
+    assert.equal(hand.getRank(), 'High Card');
+  });
+});
+
